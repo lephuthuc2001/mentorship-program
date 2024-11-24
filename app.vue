aba7a17a5aba7f91c957c075df79c89f003e0f52
Le Phu Thuc <lephuthuc2001@gmail.com>
2024-11-24 12:51:43 +0700

    feat: :package: Basic set-up

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-blue-500 text-2xl text-center font-bold">
+      Mentorship Program
+    </h1>
+  </div>
+</template>
---
+++ tests/squareroot.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('square root', () => {
+  it('finds squareroot', () => {
+    expect(Math.sqrt(49)).toBe(7)
+  })
+})
